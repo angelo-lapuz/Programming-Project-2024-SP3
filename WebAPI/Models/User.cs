0337ab5b0c7befe17d875f079fd1d5afed9d38b2
Angie <angekinz@hotmail.com>
2024-09-05 00:15:47 +1000

    Added User model.

---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int AwardID { get; set; }
+        public virtual Award Award { get; set; }
+        public string ProfileIMG { get; set; }
+        public int TaskID { get; set; }
+        public virtual Task Task { get; set; }
+
+        // TODO - can be a list of Like datatypes
+        public int Likes { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+}
