9c6536740f85d814ecca8316e1a5370ad6133645
Angie <angekinz@hotmail.com>
2024-09-05 00:08:11 +1000

    Created Award model.

---
+++ WebAPI/Models/Award.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class Award
+    {
+        public int AwardID { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+
+        // TODO - double check condition datatype
+        public string Condition { get; set; }
+    }
+}
