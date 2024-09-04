88a74681feb150acc23cd85e1b1b5ee1c3ce8b62
Angie <angekinz@hotmail.com>
2024-09-05 00:03:20 +1000

    Added more members to Task model.

--- WebAPI/Models/Task.cs
+++ WebAPI/Models/Task.cs
@@ -4,6 +4,9 @@
     {
         public int TaskID { get; set; }
         public string Name { get; set; }
-
+        public string IMG { get; set; }
+        public string Details { get; set; }
+        public string Coords { get; set; }
+        public char AccountType { get; set; }
     }
 }
