62e584685da512380a9e1729bd827415ed2b8ace
Angie <angekinz@hotmail.com>
2024-09-05 23:18:30 +1000

    Added DbContext

--- WebAPI/Data/PeakHubContext.cs
+++ WebAPI/Data/PeakHubContext.cs
@@ -1,5 +1,6 @@
 ﻿using WebAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Task = WebAPI.Models.Task;
 
 namespace WebAPI.Data
 {
@@ -8,5 +9,10 @@ namespace WebAPI.Data
         public PeakHubContext(DbContextOptions<PeakHubContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<Award> Awards { get; set; }
     }
 }
