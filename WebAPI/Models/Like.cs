e72b5f5b76a237c7c642506b9fab79734b5534ad
Angie <angekinz@hotmail.com>
2024-09-05 23:13:16 +1000

    Added constraints to models

--- WebAPI/Models/Like.cs
+++ WebAPI/Models/Like.cs
@@ -1,9 +1,14 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class Like
     {
+        [Required]
         public int PostID { get; set; }
         public virtual Post Post { get; set; }
+        [Required]
         public int UserID { get; set; }
         public virtual User User { get; set; }
         
--- WebAPI/Models/User.cs
+++ WebAPI/Models/User.cs
@@ -1,11 +1,22 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class User
     {
+        [Display(Name = "User ID")]
         public int UserID { get; set; }
+        [Required]
         public string UserName { get; set; }
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "char")]
+        [StringLength(94)]
         public string Password { get; set; }
+
         public int AwardID { get; set; }
         public virtual Award Award { get; set; }
         public string ProfileIMG { get; set; }
