caeeed9d09b98142a4342756943b61639c0566e7
Angie <angekinz@hotmail.com>
2024-09-11 00:40:49 +1000

    Created a list of Tasks in the user model.

--- WebAPI/Models/User.cs
+++ WebAPI/Models/User.cs
@@ -18,8 +18,10 @@ namespace WebAPI.Models
 
         public virtual List<Award> Awards { get; set; }
         public string ProfileIMG { get; set; }
-        public int TaskID { get; set; }
-        public virtual Task Task { get; set; }
+        //public int TaskID { get; set; }
+        //public virtual Task Task { get; set; }
+
+        public virtual List<Task> Tasks { get; set; }
 
         public virtual List<Post> Posts { get; set; }
         public virtual List<Like> Likes { get; set; }
