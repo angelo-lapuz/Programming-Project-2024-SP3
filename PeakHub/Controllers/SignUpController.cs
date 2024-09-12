772091f58ff2d10bd30fdc387b4156f594048616
Angie <angekinz@hotmail.com>
2024-09-12 23:21:44 +1000

    Added all models to Peakhub project, added SignUp Controller, and SignUp view

---
+++ PeakHub/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeakHub.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
