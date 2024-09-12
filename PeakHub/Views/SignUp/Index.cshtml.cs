41f270d4a301dcb2e2d679d3b40f3ab92a2144bb
Angie <angekinz@hotmail.com>
2024-09-12 23:21:44 +1000

    Added all models to Peakhub project, added SignUp Controller, and SignUp view

---
+++ PeakHub/Views/SignUp/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PeakHub.Views.SignUp
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
