b2efe8800f7f767085962000ca9c5a8a3d6e2220
Angie <angekinz@hotmail.com>
2024-09-13 00:50:36 +1000

    Added validation for creating a user.

--- PeakHub/ViewModels/SignUpViewModel.cs
+++ PeakHub/ViewModels/SignUpViewModel.cs
@@ -1,9 +1,24 @@
-﻿namespace PeakHub.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeakHub.ViewModels
 {
     public class SignUpViewModel
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
