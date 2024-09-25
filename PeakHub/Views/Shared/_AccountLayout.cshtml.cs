b40729fc83c445a74da454250106fc00033eabbc
Adam Saleh <adsaleh946@gmail.com>
2024-09-25 18:04:11 +1000

    Alternate design for the LogIn / SignUp
    These are based on Noah's designs in the 'noah-login-style' branch and are an attempt at a simplification + a slightly different approach.

---
+++ PeakHub/Views/Shared/_AccountLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PeakHub.Views.Shared
+{
+    public class _AccountLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
