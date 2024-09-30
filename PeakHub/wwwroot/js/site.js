850fc5ccf974f3db92eedd2440e2420c8290d93f
Fi-Boo <s2008156@student.rmit.edu.au>
2024-09-30 20:47:56 +1000

    initial styling for mobile res. see Things2Do log for notes

--- PeakHub/wwwroot/js/site.js
+++ PeakHub/wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+document.getElementById('filter-toggle').addEventListener('click', function () {
+
+    var filterOptions = document.getElementById('filter-options');
+    if (filterOptions.style.display === 'none' || filterOptions.style.display === '') {
+        filterOptions.style.display = 'grid';
+    } else {
+        filterOptions.style.display = 'none';
+    }
+
+});
+
+const difficultyMap = {
+    1: 'All',
+    2: 'Easy',
+    3: 'Moderate',
+    4: 'Hard'
+};
+
+var difficultySlider = document.getElementById('filter-difficultySlider');
+var difficultyValue = document.getElementById('difficulty-value');
+
+var difficultyInput = document.getElementById('filter-difficulty');
+
+difficultySlider.addEventListener('input', function () {
+    const difficultyLabel = difficultyMap[difficultySlider.value];
+    difficultyValue.textContent = difficultyLabel;
+    difficultyInput.value = (difficultyLabel === 'All') ? '' : difficultyLabel.charAt(0);
+});
+
+var elevationSlider = document.getElementById('filter-elevation');
+var elevationValue = document.getElementById('elevation-value');
+elevationSlider.addEventListener('input', function () {
+    elevationValue.textContent = `${elevationSlider.value} m`;
+});
+
+
+
