e0a7374f38864395b23dc7d18025d56b969e123f
Adam Saleh <adsaleh946@gmail.com>
2024-10-03 16:03:34 +1000

    Forum Beginnings
    View pages and ViewModel objects have been created. Bare minimum stuff but its a start

---
+++ PeakHub/ViewModels/ForumViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PeakHub.Models;
+
+namespace PeakHub.ViewModels {
+    public class ForumViewModel {
+        public string Username { get; set; }    // Signed In User's Username
+        public string ProfileImg { get; set; }  // Signed In User's Profile Image [OR Default Image]
+
+        public List<PostViewModel> Posts { get; set; }   // All Posts to be displayed
+        public List<int> PostsLikedByUser { get; set; } // A list of Post IDs that are liked by the user
+
+    }
+}
---
+++ PeakHub/ViewModels/PostViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PeakHub.Models;
+
+namespace PeakHub.ViewModels {
+    public class PostViewModel {
+        public string Username { get; set; }    // Post User's Username
+        public string ProfileImg { get; set; }  // Post User's Profile Image [OR Default Image]
+        public Post Post { get; set; }          // The Post to Display
+    }
+}
