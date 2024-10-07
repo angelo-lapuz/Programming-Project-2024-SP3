1d7cc380ceec9566184a4e0cd4be16e4e5c5492e
Angie <angekinz@hotmail.com>
2024-10-07 22:16:47 +1100

    Added function to get user object and place into ViewBags.

--- PeakHub/Controllers/ProfileController.cs
+++ PeakHub/Controllers/ProfileController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PeakHub.ViewModels;
+using PeakHub.Models;
 
 namespace PeakHub.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _clientFactory;
+        private HttpClient _httpClient => _clientFactory.CreateClient("api");
+
+        public ProfileController(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // gets user object from db
+            var response = await _httpClient.GetAsync($"api/users/GetUser");
+            var user = JsonConvert.DeserializeObject<User>(await response.Content.ReadAsStringAsync());
+
+            ViewBag.UserName = user.UserName;
+            ViewBag.Peaks = user.Peaks;
+            ViewBag.Awards = user.Awards;
+
             return View();
         }
     }
