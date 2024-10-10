691c885b77596af14cd2eebd0f7ec0e93b24db67
Noah Etherington <95476019+noahj97@users.noreply.github.com>
2024-10-10 14:36:34 +1100

    Noah Create
    Cant seem to get it to work, unsure if post api is working as needed?

--- PeakHub/Controllers/ForumController.cs
+++ PeakHub/Controllers/ForumController.cs
@@ -3,20 +3,29 @@ using Newtonsoft.Json;
 using PeakHub.Models;
 using PeakHub.ViewModels.Forum;
 
-namespace PeakHub.Controllers {
-    public class ForumController : Controller {
+namespace PeakHub.Controllers
+{
+    public class ForumController : Controller
+    {
         private readonly HttpClient _httpClient;
-        public ForumController(IHttpClientFactory httpClient) =>
+        private readonly ILogger<ForumController> _logger;
+        public ForumController(IHttpClientFactory httpClient, ILogger<ForumController> logger)
+        {
             _httpClient = httpClient.CreateClient("api");
+            _logger = logger;
 
-        public async Task<IActionResult> Index(int boardID) {
+        }
+
+        public async Task<IActionResult> Index(int boardID)
+        {
             if (await IsInvalidBoardID(boardID)) return RedirectToAction("Index", "Board");
 
             UserViewModel user = await GetUserDetails();
             Board board = await GetBoard(boardID);
-            List<Post> posts = board.Posts;        
+            List<Post> posts = board.Posts;
 
-            ForumViewModel viewModel = new ForumViewModel { 
+            ForumViewModel viewModel = new ForumViewModel
+            {
                 Board = board,
                 User = user,
                 Posts = await GetForumPosts(posts, user.userID)
@@ -25,13 +34,16 @@ namespace PeakHub.Controllers {
             return View(viewModel);
         }
 
-        public async Task<List<ForumPostViewModel>> GetForumPosts(List<Post> posts, string userID) {
+        public async Task<List<ForumPostViewModel>> GetForumPosts(List<Post> posts, string userID)
+        {
             List<ForumPostViewModel> viewPosts = new();
 
-            foreach(Post post in posts) {
+            foreach (Post post in posts)
+            {
                 post.User = await GetPostUser(post.UserId);
 
-                viewPosts.Add(new ForumPostViewModel { 
+                viewPosts.Add(new ForumPostViewModel
+                {
                     UserHasLiked = (await HasUserLikedPost(post.PostID, userID)),
                     LikeCount = await LikesForPost(post.PostID),
                     Post = post
@@ -41,58 +53,70 @@ namespace PeakHub.Controllers {
             return viewPosts;
         }
 
-        public async Task<Board> GetBoard(int boardID) {
+        public async Task<Board> GetBoard(int boardID)
+        {
             return await _httpClient.GetFromJsonAsync<Board>($"api/boards/{boardID}");
         }
 
-        public async Task<bool> IsInvalidBoardID(int boardID) {
+        public async Task<bool> IsInvalidBoardID(int boardID)
+        {
             int boardTotal = await _httpClient.GetFromJsonAsync<int>("api/boards/total");
             return (boardID <= 0 || boardID > boardTotal);
         }
 
-        public async Task<UserViewModel> GetUserDetails() {
+        public async Task<UserViewModel> GetUserDetails()
+        {
             var response = await _httpClient.GetAsync("api/users/GetUser");
 
-            if (response.IsSuccessStatusCode) {
+            if (response.IsSuccessStatusCode)
+            {
                 var result = await response.Content.ReadAsStringAsync();
                 User user = JsonConvert.DeserializeObject<User>(result);
 
-                if (user != null) {
-                    return new UserViewModel {
+                if (user != null)
+                {
+                    return new UserViewModel
+                    {
                         userID = user.Id,
                         username = user.UserName,
-                        profileImg = (user.ProfileIMG != null) ? 
+                        profileImg = (user.ProfileIMG != null) ?
                             user.ProfileIMG : "https://images.pexels.com/photos/6757691/pexels-photo-6757691.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                     };
                 }
             }
 
-            return new UserViewModel {
+            return new UserViewModel
+            {
                 userID = "0",
                 username = "Inconspicuous Andy",
                 profileImg = "https://images.pexels.com/photos/6757691/pexels-photo-6757691.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             };
         }
 
-        public async Task<User> GetPostUser(string userID) {
+        public async Task<User> GetPostUser(string userID)
+        {
             return await _httpClient.GetFromJsonAsync<User>($"api/users/{userID}");
         }
 
-        public async Task<bool> HasUserLikedPost(int postID, string userID) {
+        public async Task<bool> HasUserLikedPost(int postID, string userID)
+        {
             return await _httpClient.GetFromJsonAsync<bool>($"api/likes/{postID}/{userID}");
         }
 
-        public async Task<int> LikesForPost(int postID) {
+        public async Task<int> LikesForPost(int postID)
+        {
             return await _httpClient.GetFromJsonAsync<int>($"api/likes/posts/{postID}");
         }
 
         // -------------------------------------------------------------------------------- //
 
         [HttpPost]
-        public async Task<IActionResult> LikePost(int postID, string userID) {
+        public async Task<IActionResult> LikePost(int postID, string userID)
+        {
             var result = await _httpClient.PutAsJsonAsync($"api/likes/add/{postID}/{userID}", new { });
 
-            if (result.IsSuccessStatusCode) {
+            if (result.IsSuccessStatusCode)
+            {
                 int likeCount = await LikesForPost(postID);
                 return Json(new { message = "Sucess! A Brilliant Success!", likes = likeCount });
             }
@@ -100,14 +124,101 @@ namespace PeakHub.Controllers {
         }
 
         [HttpDelete]
-        public async Task<IActionResult> UnlikePost(int postID, string userID) {
+        public async Task<IActionResult> UnlikePost(int postID, string userID)
+        {
             var result = await _httpClient.DeleteFromJsonAsync<bool>($"api/likes/remove/{postID}/{userID}");
 
-            if (result) {
+            if (result)
+            {
                 int likeCount = await LikesForPost(postID);
                 return Json(new { message = "Sucess! A Brilliant Success!", likeCount });
             }
             else return Json(new { message = "Failure! A Devastating Failure!" });
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ForumViewModel viewModel)
+        {
+            if (viewModel.User == null)
+            {
+                viewModel.User = await GetUserDetails();
+            }
+
+            if (viewModel.Board == null)
+            {
+                viewModel.Board = new Board();
+            }
+
+            if (viewModel.Board.BoardID == 0)
+            {
+                ModelState.AddModelError("", "Board information is missing.");
+                return View("Index", viewModel);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View("Index", viewModel);
+            }
+
+            using (var formDataContent = new MultipartFormDataContent())
+            {
+                formDataContent.Add(new StringContent(viewModel.User.userID), "UserID");
+                formDataContent.Add(new StringContent(viewModel.Board.BoardID.ToString()), "BoardID");
+
+                if (!string.IsNullOrEmpty(viewModel.NewPostContent))
+                {
+                    formDataContent.Add(new StringContent(viewModel.NewPostContent), "Content");
+                }
+                if (viewModel.NewPostMediaFile != null && viewModel.NewPostMediaFile.Length > 0)
+                {
+                    var fileContent = new StreamContent(viewModel.NewPostMediaFile.OpenReadStream());
+                    fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(viewModel.NewPostMediaFile.ContentType);
+                    formDataContent.Add(fileContent, "mediaFile", viewModel.NewPostMediaFile.FileName);
+                }
+
+                var response = await _httpClient.PostAsync("api/posts/create", formDataContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var board = await GetBoard(viewModel.Board.BoardID);
+                    viewModel.Posts = await GetForumPosts(board.Posts, viewModel.User.userID);
+
+                    return View("Index", viewModel);
+                }
+
+                else
+                {
+                    var errorMessage = await response.Content.ReadAsStringAsync();
+                    ModelState.AddModelError(string.Empty, errorMessage);
+                }
+            }
+
+            return View("Index", viewModel);
+        }
+
+
+        private async Task<string> SaveMediaFile(IFormFile mediaFile)
+        {
+            if (mediaFile == null || mediaFile.Length == 0) return null;
+
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(mediaFile.FileName);
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await mediaFile.CopyToAsync(fileStream);
+            }
+
+            _logger.LogInformation("Media file saved at: {FilePath}", filePath);
+
+            return "/uploads/" + uniqueFileName;
+        }
     }
 }
+
--- PeakHub/ViewModels/Forum/ForumViewModel.cs
+++ PeakHub/ViewModels/Forum/ForumViewModel.cs
@@ -1,9 +1,18 @@
 ﻿using PeakHub.Models;
 
-namespace PeakHub.ViewModels.Forum {
-    public class ForumViewModel {
-        public UserViewModel User { get; set; } // Necessary User Details
-        public Board Board { get; set; } // The Board the User is currently In
-        public List<ForumPostViewModel> Posts { get; set; } // All Posts for this Board
+namespace PeakHub.ViewModels.Forum
+{
+    public class ForumViewModel
+    {
+        public UserViewModel User { get; set; }
+        public Board Board { get; set; }
+        public ForumViewModel()
+        {
+            Posts = new List<ForumPostViewModel>();
+        }
+        public string NewPostContent { get; set; }
+
+        public List<ForumPostViewModel> Posts { get; set; }
+        public IFormFile NewPostMediaFile { get; set; }
     }
 }
