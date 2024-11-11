a2c830b805b946bcc68795fe06724136aed5554a
Max8823 <80603151+Max8823@users.noreply.github.com>
2024-11-12 00:50:14 +1000

    changes to planner, styling, error messages

--- PeakHub/ViewModels/ForgotPasswordViewModel.cs
+++ PeakHub/ViewModels/ForgotPasswordViewModel.cs
@@ -4,6 +4,7 @@ namespace PeakHub.ViewModels{
     public class ForgotPasswordViewModel  {
         [Required]
         [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
     }
