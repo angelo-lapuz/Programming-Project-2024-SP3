273046bace29ce82010bbdb6669e39588ef09449
HRH The Gunzel <135931868+s3427269@users.noreply.github.com>
2024-11-12 14:09:01 +1100

    General Page basic {insert butterfly}

---
+++ PeakHub/Controllers/GeneralController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeakHub.Controllers
+{
+    public class GeneralController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
