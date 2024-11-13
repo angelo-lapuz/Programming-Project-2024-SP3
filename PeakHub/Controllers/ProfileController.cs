659e88af1d0029cbacf4a3531a7292b22ceadcf2
angelo-lapuz <s3914378@student.rmit.edu.au>
2024-11-13 12:40:52 +1100

    changed back to peaks

--- PeakHub/Controllers/ProfileController.cs
+++ PeakHub/Controllers/ProfileController.cs
@@ -35,8 +35,8 @@ namespace PeakHub.Controllers
             }
             User user = await _tools.GetUser(userID);
 
-            // get awards
-            var awards = await _httpClient.GetAsync("api/awards");
+            // get peaks
+            var peaks = await _httpClient.GetAsync("api/peaks");
 
             string defaultImg = "https://peakhub-user-content.s3.amazonaws.com/default.jpg";
             string profileImg = !string.IsNullOrEmpty(user.ProfileIMG) ? user.ProfileIMG : defaultImg;
@@ -47,7 +47,7 @@ namespace PeakHub.Controllers
             ViewBag.Awards = user.UserAwards.Select(ua => ua.Award).ToList();
             ViewBag.ProfileImg = profileImg;
             ViewBag.totalCompleted = user.UserPeaks.Count;
-            ViewBag.AllAwards = awards;
+            ViewBag.AllPeaks = peaks;
 
             return View();
         }
