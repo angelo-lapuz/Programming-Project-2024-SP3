d9d29b77ee0ba4eb2e5ea3a870e3e45fcda7a6da
Adam Saleh <adsaleh946@gmail.com>
2024-11-14 14:38:28 +1100

    Profile Edit - Done
    A few things needed to be modified, but it works.
    
    Gave me a chance to re-check profile css as well.

--- PeakHub/Controllers/ProfileController.cs
+++ PeakHub/Controllers/ProfileController.cs
@@ -4,31 +4,63 @@ using PeakHub.ViewModels;
 using PeakHub.Models;
 using System.Text;
 using PeakHub.Utilities;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace PeakHub.Controllers {
     public class ProfileController : Controller {
         private readonly IHttpClientFactory _clientFactory;
         private readonly ILogger<ProfileController> _logger;
         private readonly Tools _tools;
+        private readonly IAmazonS3 _s3;
         private HttpClient _httpClient => _clientFactory.CreateClient("api");
         private string userID => HttpContext.Session.GetString("UserId");
 
-        public ProfileController(IHttpClientFactory clientFactory, ILogger<ProfileController> logger, Tools tools)
+        public ProfileController(IHttpClientFactory clientFactory, IAmazonS3 s3, ILogger<ProfileController> logger, Tools tools)
         {
             _clientFactory = clientFactory;
             _logger = logger;
             _tools =  tools;
+            _s3 = s3;
+        }
 
+        // Get Presigned URL [Profile IMG Edit]
+        [HttpPost]
+        public IActionResult GetPresignedURL([FromBody] PresignedURLDataModel data) {
+            string imgID = data.ID, fileType = data.FileType;
+
+            if (string.IsNullOrEmpty(imgID)) return Json(new { success = false, message = "ImageID is Null" });
+            if (string.IsNullOrEmpty(userID)) return Json(new { success = false, message = "UserID is Null" });
+            if (string.IsNullOrEmpty(fileType)) return Json(new { success = false, message = "File Type is Null" });
+
+            try {
+                string fileExtension = _tools.GetFileExtension(fileType);
+
+                string key = $"{userID}/profile/{imgID}";
+                if (!string.IsNullOrEmpty(fileExtension)) { key += $"{fileExtension}"; }
+
+                var request = new GetPreSignedUrlRequest {
+                    BucketName = "peakhub-user-content",
+                    Key = key,
+                    Expires = DateTime.UtcNow.AddMinutes(60),
+                    Verb = HttpVerb.PUT,
+                    ContentType = fileType
+                };
+
+                string presignedURL = _s3.GetPreSignedURL(request);
+                Console.WriteLine($"Key = [{key}] \nURL = [{presignedURL}]");
+                return Json(new { success = true, url = presignedURL, key = key });
+            }
+            catch (Exception ex) {
+                return Json(new { success = false, message = $"Error [{ex.Message}]" });
+            }
         }
 
         // Index page for user profile - displays user details, peaks and awards
         public async Task<IActionResult> Index()
         {
             // ensures that the user is currently logged in
-            if(userID == null)
-            {
-                return RedirectToAction("Login", "Login");
-            }
+            if (userID == null) return RedirectToAction("Login", "Login");
 
             // get the current user
             User user = await _tools.GetUser(userID);
@@ -40,33 +72,27 @@ namespace PeakHub.Controllers {
 
             // set the image for the user profile
             string defaultImg = "https://peakhub-user-content.s3.amazonaws.com/default.jpg";
-            string profileImg = !string.IsNullOrEmpty(user.ProfileIMG) ? user.ProfileIMG : defaultImg;
 
             // put the user details, peaks and awards into the viewbag
             ViewBag.UserName = user.UserName;
             ViewBag.Email = user.Email;
             ViewBag.Peaks = user.UserPeaks.Select(up => up.Peak).ToList();
             ViewBag.Awards = user.UserAwards.Select(ua => ua.Award).ToList();
-            ViewBag.ProfileImg = profileImg;
+            ViewBag.ProfileImg = string.IsNullOrEmpty(user.ProfileIMG) ? defaultImg : user.ProfileIMG;
             ViewBag.totalCompleted = user.UserPeaks.Count;
             ViewBag.AllPeaks = peaks;
 
-            return View();
+            return View("Index");
         }
 
         // returns the EditDetails page for the user - allows the user to edit their details
         [HttpGet]
-        public async Task<IActionResult> EditDetails()
-        {
-            return View();
-        }
+        public async Task<IActionResult> EditDetails() => View();
 
 
         // called when the user is attempting to edit their details from the front
         [HttpPost]
-        public async Task<IActionResult> EditDetails(EditDetailsViewModel model)
-        {
-
+        public async Task<IActionResult> EditDetails(EditDetailsViewModel model) {
             // get the current user
             User user = await _tools.GetUser(userID);
 
@@ -74,17 +100,15 @@ namespace PeakHub.Controllers {
             user.FirstName = model.FirstName;
             user.LastName = model.LastName;
             user.Address = model.Address;
+            user.ProfileIMG = model.ProfileIMG;
 
             var result = await _httpClient.PutAsJsonAsync("api/users", user);
 
             // if the user was updated successfully, return the view else return an error
-            if (result.IsSuccessStatusCode)
-            {
+            if (result.IsSuccessStatusCode) {
                 TempData["SuccessMessage"] = "Your details have been successfully updated.";
-                return View(model);
-            }
-            else
-            {
+                return await Index();
+            } else {
                 return BadRequest(new { error = "Failed to update user with new route." });
             }
         }
@@ -98,36 +122,28 @@ namespace PeakHub.Controllers {
 
         // called when the user is attempting to change their password from the front
         [HttpPost]
-        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
-        {
-            // check if the model is valid
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model) {
+            if (ModelState.IsValid) {
+                model.ID = userID;
 
-            model.ID = userID; 
+                // convert viewmodel to json to be sent to api/users/ChangePassword
+                var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("api/users/ChangePassword", content);
 
-            // convert viewmodel to json to be sent to api/users/ChangePassword
-            var content = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-            var response = await _httpClient.PostAsync("api/users/ChangePassword", content);
+                // if password failed to change display error message
+                if (!response.IsSuccessStatusCode) {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    var errors = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(errorContent);
 
-            // if password failed to change display error message
-            if (!response.IsSuccessStatusCode)
-            {
-                var errorContent = await response.Content.ReadAsStringAsync();
-                var errors = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(errorContent);
-
-                foreach (var error in errors)
-                {
-                    foreach (var errorMessage in error.Value)
-                    {
-                        ModelState.AddModelError(error.Key, errorMessage);
+                    foreach (var error in errors) {
+                        foreach (var errorMessage in error.Value) {
+                            ModelState.AddModelError(error.Key, errorMessage);
+                        }
                     }
                 }
-            }
-            else{
-                TempData["SuccessMessage"] = "Password Changed Successfully.";
+                else {
+                    TempData["SuccessMessage"] = "Password Changed Successfully.";
+                }
             }
 
             return View(model);
---
+++ PeakHub/wwwroot/js/profileEdit.js
@@ -0,0 +1,81 @@
+﻿// -------------------------------------------------------------------------------- //
+import { getPresignedURL } from './s3Upload.js';
+let cropper;
+let originalMIME;
+
+$("#cropImage").on("load", function () { 
+    if (cropper) { cropper.destroy(); }
+
+    cropper = new Cropper($("#cropImage")[0], {
+        aspectRatio: 1,
+        viewMode: 1,
+        responsive: true,
+        autoCropArea: 1
+    });
+});
+
+$("#addImage").on("click", function () {
+    $("#viewCropped").html("").hide();
+    $("#imageSelector").click();
+})
+
+$("#imageSelector").on("change", function () {
+    const file = $(this)[0].files[0];
+
+    if (file) {
+        originalMIME = file.type;
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            if ($("#cropImage").attr("src") !== e.target.result) {
+                $("#cropImage").attr("src", e.target.result);
+                $("#mediaView").show();
+            }
+        }
+
+        reader.readAsDataURL(file);
+    }
+    else { $("#mediaView").hide().empty(); }
+});
+
+$("#saveImage").on("click", async function (e) {
+    e.preventDefault();
+
+    if (cropper) {
+        cropper.getCroppedCanvas().toBlob(async function (blob) {
+            if (blob) {
+                // Generate Display
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    $("#viewCropped").html(`<img src="${e.target.result}" alt="Cropped Preview">`).show();
+                }
+                reader.readAsDataURL(blob);
+
+                // Upload File
+                try {
+                    const url = await getPresignedURL("image", blob, "Profile");
+                    console.log(`URL: ${url}`);
+
+                    if (url) {
+                        console.log("Success! Image Uploaded");
+                        $("#imgLink").val(url);
+                    }
+                }
+                catch (error) {
+                    console.log(`Upload Error: ${error}`);
+                }
+            }
+        }, originalMIME);
+
+        cropper.destroy();
+        $("#cropImage").attr("src", "");
+        $("#mediaView").hide();
+    }
+});
+
+$("#cancelImage").on("click", function () {
+    if (cropper) { cropper.destroy(); }
+    $("#cropImage").attr("src", "");
+    $("#mediaView").hide();
+})
+
+// -------------------------------------------------------------------------------- //
